4a7e4c97a8027be8501bf0618cad270b0f4bd4c8
krenat <renat.kravchenko@gmail.com>
2025-01-31 11:04:04 +0200

    Update pipe_remove.sh

--- pipe/pipe_remove.sh
+++ pipe/pipe_remove.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-echo "stopping pipe processes"
+echo -e "stopping pipe processes"
 sudo systemctl stop dcdnd.service
 sudo systemctl disable dcdnd.service
 sudo rm /etc/systemd/system/dcdnd.service
 sudo systemctl daemon-reload
-echo "pipe processes stopped"
+echo -e "pipe processes stopped"
