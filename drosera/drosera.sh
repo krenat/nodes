fbc6cc6431ef692c19a94fdf3a599364ae8f4af4
krenat <renat.kravchenko@gmail.com>
2025-04-29 17:51:39 +0300

    Update drosera.sh

--- drosera/drosera.sh
+++ drosera/drosera.sh
@@ -4,90 +4,99 @@ CONFIG_FILE="$HOME/drosera/.drosera_keys"
 
 # Функція для завантаження ключів
 load_private_key() {
-    if [[ ! -f "$CONFIG_FILE" ]]; then
-        echo "❌ Файл з ключами не знайдено: $CONFIG_FILE"
-        exit 1
-    fi
-    source "$CONFIG_FILE"
-    if [[ -z "$PRIVATE_KEY" ]]; then
-        echo "❌ PRIVATE_KEY не знайдено у $CONFIG_FILE"
-        exit 1
-    fi
+  if [[ ! -f "$CONFIG_FILE" ]]; then
+    echo "❌ Файл з ключами не знайдено: $CONFIG_FILE"
+    exit 1
+  fi
+  source "$CONFIG_FILE"
+  if [[ -z "$PRIVATE_KEY" ]]; then
+    echo "❌ PRIVATE_KEY не знайдено у $CONFIG_FILE"
+    exit 1
+  fi
 }
 
 load_public_key() {
-    if [[ ! -f "$CONFIG_FILE" ]]; then
-        echo "❌ Файл з ключами не знайдено: $CONFIG_FILE"
-        exit 1
-    fi
-    source "$CONFIG_FILE"
-    if [[ -z "$PUBLIC_KEY" ]]; then
-        echo "❌ PUBLIC_KEY не знайдено у $CONFIG_FILE"
-        exit 1
-    fi
+  if [[ ! -f "$CONFIG_FILE" ]]; then
+    echo "❌ Файл з ключами не знайдено: $CONFIG_FILE"
+    exit 1
+  fi
+  source "$CONFIG_FILE"
+  if [[ -z "$PUBLIC_KEY" ]]; then
+    echo "❌ PUBLIC_KEY не знайдено у $CONFIG_FILE"
+    exit 1
+  fi
 }
 
 load_drosera_key() {
-    if [[ ! -f "$CONFIG_FILE" ]]; then
-        echo "❌ Файл з ключами не знайдено: $CONFIG_FILE"
-        exit 1
-    fi
-    source "$CONFIG_FILE"
-    if [[ -z "$DROSERA_ADDRESS" ]]; then
-        echo "❌ DROSERA_ADDRESS не знайдено у $CONFIG_FILE"
-        exit 1
-    fi
+  if [[ ! -f "$CONFIG_FILE" ]]; then
+    echo "❌ Файл з ключами не знайдено: $CONFIG_FILE"
+    exit 1
+  fi
+  source "$CONFIG_FILE"
+  if [[ -z "$DROSERA_ADDRESS" ]]; then
+    echo "❌ DROSERA_ADDRESS не знайдено у $CONFIG_FILE"
+    exit 1
+  fi
+}
+
+install() {
+	save_keys
+	install_tools
+	init_project
+
+	echo "Щоб зміни вступили в силу, виконайте:"
+	echo "source ~/.bashrc"
 }
 
 # Етап 1: Збереження ключів
 save_keys() {
-    echo "[Етап 1] Збереження приватного та публічного ключів"
-    read -p "Введіть ваш PRIVATE_KEY: " PRIVATE_KEY
-    read -p "Введіть ваш PUBLIC_KEY: " PUBLIC_KEY
+  echo "[Етап 1] Збереження приватного та публічного ключів"
+  read -p "Введіть ваш PRIVATE_KEY: " PRIVATE_KEY
+  read -p "Введіть ваш PUBLIC_KEY: " PUBLIC_KEY
 
-    mkdir -p "$(dirname "$CONFIG_FILE")"
+  mkdir -p "$(dirname "$CONFIG_FILE")"
 
-    echo "PRIVATE_KEY=\"$PRIVATE_KEY\"" > "$CONFIG_FILE"
-    echo "PUBLIC_KEY=\"$PUBLIC_KEY\"" >> "$CONFIG_FILE"
+  echo "PRIVATE_KEY=\"$PRIVATE_KEY\"" > "$CONFIG_FILE"
+  echo "PUBLIC_KEY=\"$PUBLIC_KEY\"" >> "$CONFIG_FILE"
 
-    echo "✅ Ключі збережено у $CONFIG_FILE"
+  echo "✅ Ключі збережено у $CONFIG_FILE"
 }
 
 # Етап 2: Встановлення інструментів та ініціалізація проєкту
 install_tools() {
-    echo "[Етап 2] Встановлення Foundry, Bun, Drosera і ініціалізація проєкту"
+  echo "[Етап 2] Встановлення Foundry, Bun, Drosera і ініціалізація проєкту"
 
-    cd ~
+  cd ~
 
-    # Foundry
-    curl -L https://foundry.paradigm.xyz | bash
-	export PATH="$HOME/.foundry/bin:$PATH"
-    echo 'export PATH="$HOME/.foundry/bin:$PATH"' >> ~/.bashrc
-    source ~/.bashrc
-    foundryup
+  # Foundry
+  curl -L https://foundry.paradigm.xyz | bash
+  export PATH="$HOME/.foundry/bin:$PATH"
+  echo 'export PATH="$HOME/.foundry/bin:$PATH"' >> ~/.bashrc
+  source ~/.bashrc
+  foundryup
 
-    # Bun
-    curl -fsSL https://bun.sh/install | bash
-	export PATH="$HOME/.bun/bin:$PATH"
-    echo 'export PATH="$HOME/.bun/bin:$PATH"' >> ~/.bashrc
+  # Bun
+  curl -fsSL https://bun.sh/install | bash
+  export PATH="$HOME/.bun/bin:$PATH"
+  echo 'export PATH="$HOME/.bun/bin:$PATH"' >> ~/.bashrc
 
-    # Drosera
-    curl -L https://app.drosera.io/install | bash
-	export PATH="$HOME/.drosera/bin:$PATH"
-    echo 'export PATH="$HOME/.drosera/bin:$PATH"' >> ~/.bashrc
-    droseraup
+  # Drosera
+  curl -L https://app.drosera.io/install | bash
+  export PATH="$HOME/.drosera/bin:$PATH"
+  echo 'export PATH="$HOME/.drosera/bin:$PATH"' >> ~/.bashrc
+  droseraup
 
-    # Створення директорії
-    mkdir -p ~/my-drosera-trap
-    cd ~/my-drosera-trap
+  # Створення директорії
+  mkdir -p ~/my-drosera-trap
+  cd ~/my-drosera-trap
 
-    # Forge ініціалізація
-    forge init -t drosera-network/trap-foundry-template
+  # Forge ініціалізація
+  forge init -t drosera-network/trap-foundry-template
 
-    # Компіляція
-    bun install
-    forge build
-	
+  # Компіляція
+  bun install
+  forge build
+
 	cd ~
 	echo "Щоб зміни вступили в силу, виконайте:"
 	echo "source ~/.bashrc"
@@ -96,8 +105,8 @@ install_tools() {
 init_project() {
 	echo "[Етап 3] ініціалізація проєкту"
 
-    # Застосування drosera apply
-    apply_drosera
+  # Застосування drosera apply
+  apply_drosera
 
 	# Тепер парсимо адресу
 	trap_address=$(grep '^address' $HOME/my-drosera-trap/drosera.toml | sed -E 's/.*"([^"]+)"/\1/')
@@ -108,8 +117,8 @@ init_project() {
 		echo "Не вдалося знайти адресу у виводі drosera apply."
 		exit 1
 	fi
-	
-    boost_drosera
+
+  boost_drosera
 
 	DROSERA_TOML_PATH="$HOME/my-drosera-trap/drosera.toml"
 
@@ -131,132 +140,142 @@ init_project() {
 		echo -e "whitelist = [\"$PUBLIC_KEY\"]" >> "$DROSERA_TOML_PATH"
 	fi
 	echo "Оновлення drosera.toml завершено!"
-	sleep 2
+	sleep 5
 	apply_drosera
 }
 
 # Етап 4: Завантаження drosera-operator та налаштування firewall
 setup_operator_and_firewall() {
-    echo "[Етап 4] Завантаження drosera-operator і налаштування Firewall"
+  echo "[Етап 4] Завантаження drosera-operator і налаштування Firewall"
 
-    load_private_key
-    cd ~
+  load_private_key
+  cd ~
 
-    # Завантаження drosera-operator
-    curl -LO https://github.com/drosera-network/releases/releases/download/v1.16.2/drosera-operator-v1.16.2-x86_64-unknown-linux-gnu.tar.gz
-    tar -xvf drosera-operator-v1.16.2-x86_64-unknown-linux-gnu.tar.gz
+  # Завантаження drosera-operator
+  curl -LO https://github.com/drosera-network/releases/releases/download/v1.16.2/drosera-operator-v1.16.2-x86_64-unknown-linux-gnu.tar.gz
+  tar -xvf drosera-operator-v1.16.2-x86_64-unknown-linux-gnu.tar.gz
 
-    # Docker
-    docker pull ghcr.io/drosera-network/drosera-operator:latest
+  # Docker
+  docker pull ghcr.io/drosera-network/drosera-operator:latest
 
-    # Firewall
-    echo "[Firewall] Увімкнення UFW та відкриття портів..."
-    sudo ufw allow ssh
-    sudo ufw allow 22
-    sudo ufw allow 31313/tcp
-    sudo ufw allow 31314/tcp
-    sudo ufw --force enable
+  # Firewall
+  echo "[Firewall] Увімкнення UFW та відкриття портів..."
+  sudo ufw allow ssh
+  sudo ufw allow 22
+  sudo ufw allow 31313/tcp
+  sudo ufw allow 31314/tcp
+  sudo ufw --force enable
 
-    # Реєстрація
-    ./drosera-operator register \
-        --eth-rpc-url https://ethereum-holesky-rpc.publicnode.com \
-        --eth-private-key "$PRIVATE_KEY"
+  # Реєстрація
+  ./drosera-operator register \
+      --eth-rpc-url https://ethereum-holesky-rpc.publicnode.com \
+      --eth-private-key "$PRIVATE_KEY"
 }
 
 # Етап 5: Клонування Drosera-Network
 clone_drosera_network() {
-    echo "[Етап 5] Клонування Drosera-Network"
+  echo "[Етап 5] Клонування Drosera-Network"
 
-    cd ~
-    git clone https://github.com/0xmoei/Drosera-Network
-	
-	echo "[Етап 5] Створення і заповнення .env файлу"
+  cd ~
+  git clone https://github.com/0xmoei/Drosera-Network
 
-    cd ~/Drosera-Network
-    cp .env.example .env
+  echo "[Етап 5] Створення і заповнення .env файлу"
 
-    load_private_key
+  cd ~/Drosera-Network
+  cp .env.example .env
 
-    VPS_IP=$(curl -s ifconfig.me)
-    if [[ -z "$VPS_IP" ]]; then
-        echo "❌ Не вдалося отримати публічну IP адресу"
-        exit 1
-    fi
+  load_private_key
 
-    sed -i "s|^ETH_PRIVATE_KEY=.*|ETH_PRIVATE_KEY=$PRIVATE_KEY|" .env
-    sed -i "s|^VPS_IP=.*|VPS_IP=$VPS_IP|" .env
+  VPS_IP=$(curl -s ifconfig.me)
+  if [[ -z "$VPS_IP" ]]; then
+      echo "❌ Не вдалося отримати публічну IP адресу"
+      exit 1
+  fi
 
-    echo "✅ .env оновлено:"
-    echo "  ETH_PRIVATE_KEY=$PRIVATE_KEY"
-    echo "  VPS_IP=$VPS_IP"
+  sed -i "s|^ETH_PRIVATE_KEY=.*|ETH_PRIVATE_KEY=$PRIVATE_KEY|" .env
+  sed -i "s|^VPS_IP=.*|VPS_IP=$VPS_IP|" .env
 
-    echo "[Етап 6] Запуск docker-compose і optin"
+  echo "✅ .env оновлено:"
+  echo "  ETH_PRIVATE_KEY=$PRIVATE_KEY"
+  echo "  VPS_IP=$VPS_IP"
 
-    cd ~/Drosera-Network
-    docker-compose up -d
+  echo "[Етап 6] Запуск docker-compose і optin"
 
-    optin_drosera
+  cd ~/Drosera-Network
+  docker-compose up -d
+
+  optin_drosera
 }
 
 apply_drosera() {
 	load_private_key
 	cd ~/my-drosera-trap
-	
+
 	DROSERA_PRIVATE_KEY="$PRIVATE_KEY" drosera apply
-	
+
 	cd ~
 }
 
 boost_drosera() {
   load_private_key
   load_drosera_key
- 
+
   cd ~/my-drosera-trap
-	
+
   read -p "Введіть кількість ETH: " ETH_AMOUNT
   DROSERA_PRIVATE_KEY="$PRIVATE_KEY" drosera bloomboost --trap-address "$DROSERA_ADDRESS" --eth-amount "$ETH_AMOUNT"
-  
+
   cd ~
 }
 
 optin_drosera() {
-	load_private_key
+  	load_private_key
 	load_drosera_key
- 
+
 	cd ~/Drosera-Network
-	
+
   drosera-operator optin \
         --eth-rpc-url https://ethereum-holesky-rpc.publicnode.com \
         --eth-private-key "$PRIVATE_KEY" \
         --trap-config-address "$DROSERA_ADDRESS"
-  
+
 	cd ~
 }
 
-# === Головне меню ===
-echo "Оберіть етап для виконання:"
-echo "1 — Зберегти приватний та публічний ключ у файл"
-echo "2 — Встановити Foundry, Bun, Drosera, ініціалізувати проєкт"
-echo "3 — Iніціалізувати проєкт"
-echo "4 — Завантажити drosera-operator, налаштувати firewall і зареєструвати ноду"
-echo "5 — Клонувати Drosera-Network та Заповнити .env, Запустити docker-compose і optin"
-echo "00 - Apply"
-echo "01 - boost"
-echo "02 - optin"
+show_menu() {
+	# === Головне меню ===
+	echo "Оберіть етап для виконання:"
+	echo "0 - Інсталювати поетапно"
+	echo "1 — Зберегти приватний та публічний ключ у файл"
+	echo "2 — Встановити Foundry, Bun, Drosera, ініціалізувати проєкт"
+	echo "3 — Iніціалізувати проєкт"
+	echo "4 — Завантажити drosera-operator, налаштувати firewall і зареєструвати ноду"
+	echo "5 — Клонувати Drosera-Network та Заповнити .env, Запустити docker-compose і optin"
+	echo "00 - Apply"
+	echo "01 - boost"
+	echo "02 - optin"
+}
 
-read -p "Ваш вибір (1-4): " step
+handle_step() {
+	case "$1" in
+		0) install ;;
+		1) save_keys ;;
+		2) install_tools ;;
+		3) init_project ;;
+		4) setup_operator_and_firewall ;;
+		5) clone_drosera_network ;;
+		00) apply_drosera ;;
+		01) boost_drosera ;;
+		02) optin_drosera ;;
+		*) show_menu ;;
+	esac
+}
 
-case "$step" in
-    1) save_keys ;;
-    2) install_tools ;;
-	3) init_project ;;
-    4) setup_operator_and_firewall ;;
-    5) clone_drosera_network ;;
-  	00) apply_drosera ;;
-   	01) boost_drosera ;;
-    02) optin_drosera ;;
-    *)
-        echo "❌ Невірний вибір. Введіть число від 1 до 4."
-        exit 1
-        ;;
-esac
+if [[ -n "$1" ]]; then
+  handle_step "$1"
+	exit
+fi
+
+show_menu
+read -r -p "Ваш вибір (1-4): " step
+handle_step $step
