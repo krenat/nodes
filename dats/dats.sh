07959647866a057aa61a712ce38c2d19a8dfa714
renat <renat.kravchenko@gmail.com>
2025-05-08 19:55:55 +0300

    update dats script

--- dats/dats.sh
+++ dats/dats.sh
@@ -30,33 +30,37 @@ prepare() {
     printf "\r[%s%s] %d/%d" "${fill// /#}" "${empty// /-}" "$progress" "$TOTAL"
   }
 
+  echo
+
   for pkg in "${packages[@]}"; do
     IFS=":" read -r pkg_name pkg_real <<< "$pkg"
-      check_and_install "$pkg_name" "$pkg_real"
+    check_and_install "$pkg_name" "$pkg_real"
     ((CURRENT++))
     draw_progress "$CURRENT"
   done
+
+  echo -e "\n${GREEN}✅ Усі залежності встановлені.${NC}"
 }
 
 check_and_install() {
   if ! dpkg -s "$1" &> /dev/null; then
-      echo "$1 не знайдено. Встановлюємо..."
-      sudo apt install -y "$2" >/dev/null 2>&1
+    echo -e "${YELLOW}$pkg_name не знайдено. Встановлюємо...${NC}"
+    sudo apt install -y "$2" >/dev/null 2>&1
   fi
 }
 
 download_and_install_dats() {
-  echo -e "${BLUE}Завантаження та сстановлення dats...${NC}"
+  echo -e "${BLUE}Завантаження та встановлення dats...${NC}"
   cd "$PROJECT_DIR"
   wget --show-progress -O dats_install.deb https://dl.datsproject.io/evm-linux-deb
-  sudo apt install ./dats_install.deb
+  sudo apt install ./dats_install.deb >/dev/null 2>&1
   cd
 }
 
 show_menu() {
 	echo "Оберіть етап для виконання:"
 	echo "p - інсталювати необхідні залежності"
-	echo "d - завантажити та інсталювати файл проєкту"
+	echo "i - завантажити та інсталювати файл проєкту"
 	echo "x - завершити"
 
 	read -r -p "Ваш вибір: " step
@@ -66,7 +70,7 @@ show_menu() {
 handle_step() {
 	case "$1" in
 		p) prepare ;;
-    d) download_and_install_dats ;;
+    i) download_and_install_dats ;;
     x) exit ;;
     *) show_menu ;;
 	esac
